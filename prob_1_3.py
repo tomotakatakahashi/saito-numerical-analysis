b5024cb99bd34a5b10770626d981824609f3d3a0
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2018-02-25 11:25:37 +0900

    modify 1.3

--- prob_1_3.py
+++ prob_1_3.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def f(x):
-    return sum(x*x*x) - 3
+    return sum(x*x) - 3
 
 def g(x):
     return x[0]*x[1] - 1
@@ -12,21 +12,21 @@ def f_g(x):
     return np.array([f(x), g(x)])
 
 def J_inv(x):
-    J = np.array([[3*(x[0]**2), 3*(x[1]**2)], [x[1], x[0]]])
+    J = np.array([[2*x[0], 2*x[1]], [x[1], x[0]]])
     return np.linalg.inv(J)
 
-def newton():
-    x = np.array([1, 2])
+def newton(x0):
+    x = x0
     for i in range(10):
         x = x - J_inv(x).dot(f_g(x))
         print("x = {}, f(x) = {}, g(x) = {}".format(x, f(x), g(x)))
 
 
 def f_1var(x):
-    return x**3 + x**(-3) - 3
+    return x**2 + x**(-2) - 3
 
 def f_1var_prime(x):
-    return 3*(x**2) - 3 * ( x**(-4))
+    return 2*x - 2 * ( x**(-3))
 
 def phi(x):
     return x - f_1var(x)/f_1var_prime(x)
@@ -38,7 +38,7 @@ def newton_1var():
         print("x = {}, y = {}, f(x) = {}".format(x, 1/x, f_1var(x)))
         
 def main():
-    newton()
+    newton(x0 = np.array([2, 1]))
     print("----------------------------")
     newton_1var()
     
